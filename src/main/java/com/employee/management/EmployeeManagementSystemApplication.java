f8f8ac87726ebeead68a8940e1669756be0834a0
kajal <kajal.korgaonkar99@gmail.com>
2024-06-05 12:21:45 +0530

    Trial commit

--- src/main/java/com/employee/management/EmployeeManagementSystemApplication.java
+++ src/main/java/com/employee/management/EmployeeManagementSystemApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmployeeManagementSystemApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(EmployeeManagementSystemApplication.class, args);
+        System.out.println("Employee Management System Application Started");
     }
 
 }
